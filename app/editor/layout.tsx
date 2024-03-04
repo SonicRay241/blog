af1d3773b431b4e5e3bae34afc0a1b77cb4d1479
SonicRay241 <rayhan.permana@yahoo.com>
2024-03-05 00:19:58 +0700

    testing new cookie package - 2

--- app/editor/layout.tsx
+++ app/editor/layout.tsx
@@ -1,5 +1,4 @@
 import EditorNavBar from "@/components/editor/EditorNavbar";
-import { getCookies } from 'next-client-cookies/server';
 
 export default function EditorLayout({children,}
 : Readonly<{
