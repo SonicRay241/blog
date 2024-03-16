b279211e9c0b3dd00a4d90aa138cf67547d94094
SonicRay241 <rayhan.permana@yahoo.com>
2024-03-16 19:47:12 +0700

    Cleaned up the code for reader space

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,10 @@
-"use client"
-
 import NavBar from "@/components/Navbar";
-import Link from "next/link";
-import Skeleton from "@/components/Skeleton";
-// import Footer from "@/components/Footer";
-import { url } from "@/libs/url";
 import dynamic from "next/dynamic";
-import useSWR from "swr";
-import { fetcherJSON } from "@/libs/fetchers";
-import BlogCard from "@/components/editor/BlogCard";
+import LatestBlogs from "@/components/home/LatestBlogs";
 
 const Footer = dynamic(() => import("@/components/Footer"))
 
 const Page = () => {
-  const {data: latestBlogs, error: isError, isLoading} = useSWR<{
-    created_at: string;
-    id: string;
-    title: string;
-    writer: string;
-  }[]>(`${url}/v1/blogs/latest`, fetcherJSON)
-  
-  if (!isError)
   return (
     <>
       <NavBar/>
@@ -34,58 +18,10 @@ const Page = () => {
             </h1>
             <p className="mt-10 text-center text-gray-500">Unleashing creativity in the world of technology and igniting innovation in the digital realm.</p>
           </div>
-          <h1 className="mt-24 text-gray-500 font-medium">Latest Posts</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 w-full mt-4 gap-6 mb-40">
-            {(!isLoading && latestBlogs) ? 
-              latestBlogs.map((blogData, n)=>{
-                return (
-                  // <Link 
-                  //   href={`/blog?p=${blogData.id}`} 
-                  //   className="group p-6 rounded-lg border bg-white hover:bg-gray-200 drop-shadow-sm"
-                  //   key={n}
-                  //   style={{
-                  //     transition: "all 100ms cubic-bezier(0.37, 0, 0.63, 1)"
-                  //   }}
-                  // >
-                  //   <div className="flex flex-col h-fit w-full justify-between">
-                  //     <h1 className="font-medium text-black">{blogData.title}</h1>
-                  //     <p 
-                  //       className="font-medium text-gray-500 text-xs mt-6"
-                  //     >
-                  //       {blogData.created_at.split("T")[0].split("-").reverse().join(".")} | {blogData.writer}
-                  //     </p>
-                  //   </div>
-                  // </Link>
-                  <BlogCard
-                    key={blogData.id}
-                    id={blogData.id}
-                    created_at={blogData.created_at}
-                    title={blogData.title}
-                    writer={blogData.writer}
-                  />
-                )
-              })
-              :
-              [...Array(6)].map((_, n)=>{
-                return (
-                  <Skeleton width={"full"} height={84} key={n}/>
-                )
-              })
-            }
-          </div>
+          <LatestBlogs/>
         </div>
       </div>
       <Footer/>
-    </>
-  )
-  return (
-    <>
-      <NavBar/>
-      <div className="h-3/4 w-full flex justify-center items-center">
-        <div className="flex flex-col items-center">
-          <h1 className="text-xl text-black">Something went wrong.</h1>
-        </div>
-      </div>
     </>
   )
 }
